be2e27a62e12524928244962148b510be3325a9a
Tan Vo <tan.vopm@gmail.com>
2022-06-07 22:50:28 +0700

    add form modal

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import pokemonReducers from "../features/pokemons/pokemonSlice";
-// import pokemonReducer from "../features/pokemons/pokemonSlice"
-
-// console.log(pokemonReducer)
 const store = configureStore({
     reducer: {
         pokemons: pokemonReducers
---
+++ src/components/form/FTextField.js
@@ -0,0 +1,26 @@
+import { useFormContext, Controller } from "react-hook-form";
+import { TextField } from "@mui/material";
+
+function FTextField({ name, ...other }) {
+    const { control } = useFormContext();
+
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({ field, fieldState: { error } }) => {
+                console.log(field)
+                return <TextField
+                    {...field}
+                    fullWidth
+                    error={!!error}
+                    helperText={error?.message}
+                    {...other}
+                />
+            }
+            }
+        />
+    );
+}
+
+export default FTextField;
