db005b2fec3e11fa413f144c8d0b058e7d608918
Vinh Duong <xvinh.ir@gmail.com>
2022-06-08 12:13:55 +0700

    details

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Footer } from './layouts/MainFooter';
 import Header from './layouts/MainHeader';
-import { PageTitle } from './components/PageTitile';
+import { PageTitle } from './components/PageTitle';
 import PokeList from './components/PokeList';
 import { SearchBox } from './components/SearchBox';
 import { MThemeProvider } from './themeContext/MThemeProvider';
@@ -13,24 +13,22 @@ import { HomePage } from './pages/HomePage';
 import { DetailPage } from './pages/DetailPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
-
-
 function App() {
-  return (
-    <div className="App">
-      <MThemeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              <Route index element={<HomePage />} />
-              <Route path="/:name" element={<DetailPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </MThemeProvider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MThemeProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<MainLayout />}>
+                            <Route index element={<HomePage />} />
+                            <Route path="/:id" element={<DetailPage />} />
+                            <Route path="*" element={<NotFoundPage />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </MThemeProvider>
+        </div>
+    );
 }
 
 export default App;
